b4985cc0e795ef1b922edc833a44303e60e2f2e7
Peter_Mitrano <mitranopeter@gmail.com>
2016-02-14 03:47:52 -0500

    starting on the plugin

---
+++ plugin/maze_plugin.cpp
@@ -0,0 +1,36 @@
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <stdio.h>
+
+namespace gazebo
+{
+  class MazePlugin : public ModelPlugin
+  {
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+    {
+      // Store the pointer to the model
+      this->model = _parent;
+
+      // Listen to the update event. This event is broadcast every
+      // simulation iteration.
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+          boost::bind(&MazePlugin::OnUpdate, this, _1));
+    }
+
+    // Called by the world update start event
+    public: void OnUpdate(const common::UpdateInfo & /*_info*/)
+    {
+    }
+
+    // Pointer to the model
+    private: physics::ModelPtr model;
+
+    // Pointer to the update event connection
+    private: event::ConnectionPtr updateConnection;
+  };
+
+  // Register this plugin with the simulator
+  GZ_REGISTER_MODEL_PLUGIN(MazePlugin)
+}
