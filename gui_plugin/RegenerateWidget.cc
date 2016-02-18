47979b91265625371651840963b4c736bdafa769
Peter_Mitrano <mitranopeter@gmail.com>
2016-02-18 14:35:16 -0500

    added text edit to gui overlay

---
+++ gui_plugin/RegenerateWidget.cc
@@ -0,0 +1,63 @@
+#include <sstream>
+#include <gazebo/msgs/msgs.hh>
+#include "RegenerateWidget.hh"
+
+using namespace gazebo;
+
+// Register this plugin with the simulator
+GZ_REGISTER_GUI_PLUGIN(RegenerateWidget)
+
+/////////////////////////////////////////////////
+RegenerateWidget::RegenerateWidget()
+  : GUIPlugin()
+{
+  this->counter = 0;
+
+  // Set the frame background and foreground colors
+  this->setStyleSheet(
+      "QFrame { background-color : rgba(100, 100, 100, 255); color : white; }");
+
+  // Create the main layout
+  QHBoxLayout *mainLayout = new QHBoxLayout;
+  QFrame *mainFrame = new QFrame();
+  QVBoxLayout *frameLayout = new QVBoxLayout();
+
+  QPushButton *button = new QPushButton(tr("Regenerate Maze"));
+  connect(button, SIGNAL(clicked()), this, SLOT(OnButton()));
+
+  QTextEdit *textEdit = new QTextEdit(tr("~/Projects/gzmaze/maze.mz"));
+  textEdit->setContentsMargins(1, 1, 1, 1);
+  textEdit->setAlignment(Qt::AlignBottom | Qt::AlignLeft);
+  textEdit->setObjectName("maze_filename");
+  textEdit->setFixedHeight(30);
+
+  frameLayout->addWidget(button);
+  frameLayout->addWidget(textEdit);
+  mainFrame->setLayout(frameLayout);
+  mainLayout->addWidget(mainFrame);
+
+  frameLayout->setContentsMargins(2, 2, 2, 2);
+  mainLayout->setContentsMargins(0, 0, 0, 0);
+
+  this->setLayout(mainLayout);
+
+  this->move(10, 10);
+  this->resize(250, 90);
+
+  this->node = transport::NodePtr(new transport::Node());
+  this->node->Init();
+  this->regenPub = this->node->Advertise<msgs::Factory>("~/maze/regenerate");
+}
+
+/////////////////////////////////////////////////
+RegenerateWidget::~RegenerateWidget()
+{
+}
+
+/////////////////////////////////////////////////
+void RegenerateWidget::OnButton()
+{
+  msgs::GzString msg;
+  msg.set_data(maze_filename);
+  this->regenPub->Publish(msg);
+}
---
+++ gui_plugin/RegenerateWidget.hh
@@ -0,0 +1,39 @@
+#ifndef _GUI_EXAMPLE_SPAWN_WIDGET_HH_
+#define _GUI_EXAMPLE_SPAWN_WIDGET_HH_
+
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/gui/GuiPlugin.hh>
+#ifndef Q_MOC_RUN  // See: https://bugreports.qt-project.org/browse/QTBUG-22829
+# include <gazebo/transport/transport.hh>
+# include <gazebo/gui/gui.hh>
+#endif
+
+namespace gazebo
+{
+    class GAZEBO_VISIBLE RegenerateWidget : public GUIPlugin
+    {
+      Q_OBJECT
+
+      /// \brief Constructor
+      /// \param[in] _parent Parent widget
+      public: RegenerateWidget();
+
+      /// \brief Destructor
+      public: virtual ~RegenerateWidget();
+
+      /// \brief Callback trigged when the button is pressed.
+      protected slots: void OnButton();
+
+      /// \brief Counter used to create unique model names
+      private: unsigned int counter;
+
+      /// \brief Node used to establish communication with gzserver.
+      private: transport::NodePtr node;
+
+      /// \brief Publisher of factory messages.
+      private: transport::PublisherPtr regenPub;
+
+      private: std::string maze_filename;
+    };
+}
+#endif
