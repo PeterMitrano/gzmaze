8c42ddda39f06386c34a660ff470d71e4bb270b4
Peter_Mitrano <mitranopeter@gmail.com>
2016-02-23 03:11:26 -0500

    Got basic publishing/subscribing in mouse plugin
    also bumped up model frame a bit for adding to maze

--- model_plugin/MousePlugin.cc
+++ model_plugin/MousePlugin.cc
@@ -12,7 +12,7 @@ void MousePlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf) {
   node->Init();
   control_sub = node->Subscribe("~/mouse/control", &MousePlugin::ControlCallback, this);
   pose_pub = node->Advertise<msgs::Pose>("~/mouse/pose");
-  sense_pub = node->Advertise<msgs::LaserScan>("~/mouse/sense");
+  sense_pub = node->Advertise<msgs::GzString>("~/mouse/sense");
 
   // Connect to the world update event.
   // This will trigger the Update function every Gazebo iteration
@@ -21,7 +21,28 @@ void MousePlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf) {
 }
 
 void MousePlugin::Update(const common::UpdateInfo &info) {
+  msgs::Vector3d *pos = new msgs::Vector3d();
+  pos->set_x(0);
+  pos->set_y(1);
+  pos->set_z(2);
+
+  msgs::Quaternion *rot = new msgs::Quaternion();
+  rot->set_x(0);
+  rot->set_y(0);
+  rot->set_z(0);
+  rot->set_w(1);
+
+  msgs::Pose pose;
+  pose.set_allocated_position(pos);
+  pose.set_allocated_orientation(rot);
+
+  msgs::GzString scan;
+  scan.set_data("1010");
+
+  sense_pub->Publish(scan);
+  pose_pub->Publish(pose);
 }
 
 void MousePlugin::ControlCallback(ConstGzStringPtr &msg) {
+  gzmsg << msg->data() << std::endl;
 }
