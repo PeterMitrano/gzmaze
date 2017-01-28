b334cc6245f796fbadc88080e8c9c7d5e1a6af15
Peter Mitrano <mitranopeter@gmail.com>
2017-01-27 20:04:19 -0500

    generalize  setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,5 @@
-export GAZEBO_PLUGIN_PATH=${HOME}/Projects/smartmouse/gzmaze/world_plugin/build:\
-${HOME}/Projects/smartmouse/gzmaze/gui_plugin/build:\
-${HOME}/Projects/smartmousegzmaze/model_plugin/build:\
-/usr/lib/x86_64-linux-gnu/gazebo-7/plugins:\
-$GAZEBO_PLUGIN_PATH
-
-export LD_LIBRARY_PATH=/usr/local/lib\
-/usr/local/lib/x86_64-linux-gnu\
-${HOME}/Projects/smartmouse/gzmaze/world_plugin/build:\
-${HOME}/Projects/smartmouse/gzmaze/gui_plugin/build:\
-${HOME}/Projects/smartmouse/gzmaze/model_plugin/build:\
-/usr/local/lib/x86_64-linux-gnu/gazebo-7/plugins:\
-$LD_LIBRARY_PATH
-
-export GAZEBO_MODEL_PATH=${HOME}/Projects/smartmouse/gzmaze
+export GAZEBO_PLUGIN_PATH=${PWD}/build:$GAZEBO_PLUGIN_PATH
+export LD_LIBRARY_PATH=${PWD}/build:$LD_LIBRARY_PATH
+export GAZEBO_MODEL_PATH=${PWD}
 
 gazebo -u --verbose gzmaze.world
